092489e3a860d0e8fbcacd0900d7d76e2dd80d59
Pawel Karpinski <karpinsk.pawel@gmail.com>
2021-03-27 16:54:54 +0000

    Init commit

---
+++ src/main/java/pl/bootcamp/ex21_1/Ex211Application.java
@@ -0,0 +1,13 @@
+package pl.bootcamp.ex21_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex211Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ex211Application.class, args);
+    }
+
+}
